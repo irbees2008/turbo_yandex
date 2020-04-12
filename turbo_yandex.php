9623534f4519b626844d0d28ef6d95bb7c151043
Рустам <rusiq@list.ru>
2020-04-12 22:53:25 +1000

    Вынесены все глобалки из класса `TurboYandex`

--- turbo_yandex.php
+++ turbo_yandex.php
@@ -16,6 +16,7 @@ if (! function_exists('news_showlist')) {
 loadPluginLibrary('turbo_yandex', false);
 
 use Plugins\TurboYandex;
+use function Plugins\trans;
 
 // Регистрация страниц плагина.
 register_plugin_page('turbo_yandex', '', 'plugin_turbo_yandex', 0);
@@ -23,7 +24,18 @@ register_plugin_page('turbo_yandex', 'category', 'plugin_turbo_yandex', 0);
 
 function plugin_turbo_yandex(array $params = [])
 {
+    global $SUPRESS_TEMPLATE_SHOW, $SUPRESS_MAINBLOCK_SHOW;
+
+    // Disable executing of `index` action (widget plugins and so on..)
+    actionDisable('index');
+
+    // Suppress templates
+    $SUPRESS_TEMPLATE_SHOW = 1;
+    $SUPRESS_MAINBLOCK_SHOW = 1;
+
 	$turboYandex = new TurboYandex($params);
 
-    $turboYandex->generate();
+    header("Content-Type: text/xml; charset=".trans('encoding'));
+
+    echo $turboYandex->cachedContent();
 }
